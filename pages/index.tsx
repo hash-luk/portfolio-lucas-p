9e9e5818b656042f6d7b6b713ae52df9f0299e2d
hash-luk <lucasgamerhd18@gmail.com>
2022-04-10 12:37:23 -0300

    Updated component source

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next'
-import TopIcons from './components/banner/top-icons'
-import BannerContent from './components/banner/main-content'
-import AboutMe from './components/about-me'
-import TechStack from './components/tech-stack'
-import Projects from './components/projects'
+import TopIcons from '../public/components/banner/top-icons'
+import BannerContent from '../public/components/banner/main-content'
+import AboutMe from '../public/components/about-me'
+import TechStack from '../public/components/tech-stack'
+import Projects from '../public/components/projects'
 import GlobalStyle from '../styles/globalStyles'
-import Footer from './components/footer/inde'
+import Footer from '../public/components/footer'
 
 const Home: NextPage = () => {
   return (
