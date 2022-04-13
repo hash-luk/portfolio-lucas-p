7be46d41247ecc8fa47387e201b01dd281f19789
hash-luk <lucasgamerhd18@gmail.com>
2022-04-13 08:07:45 -0300

    Added function to buttons and fix bugs

--- public/components/contact/index.tsx
+++ public/components/contact/index.tsx
@@ -1,25 +1,45 @@
 import * as S from './styles'
+import { useRef,useEffect, MutableRefObject } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagramSquare,faLinkedin,faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Contact = () => {
+    const instagramButton = useRef() as MutableRefObject<HTMLDivElement>
+    const emailButton = useRef() as MutableRefObject<HTMLDivElement>
+    const linkeinButton = useRef() as MutableRefObject<HTMLDivElement>
+    const githubButton = useRef() as MutableRefObject<HTMLDivElement>
+
+    function handleClick(destination:string) {
+        if(destination === 'instagram'){
+            window.open('https://instagram.com/lucasp_dev','_blank')
+        } else if(destination === 'email'){ 
+            let email = 'lucas.patrick2506@gmail.com'
+            navigator.clipboard.writeText(email)
+            console.log('copiado')
+        } else if(destination === 'linkedin'){
+            window.open('https://linkedin.com/in/lucas-patrick-p','_blank')
+        } else if(destination === 'github'){
+            window.open('https://github.com/hash-luk','_blank')
+        }
+    }
+ 
     return(
         <S.Container id='contact'>
             <div className="content">
-                <div className="contact instagram">
+                <div className="contact instagram" ref={instagramButton} onClick={()=> handleClick(instagramButton.current.title)} title='instagram'>
                     <p>instagram</p>
                     <FontAwesomeIcon icon={faInstagramSquare} id="instagram" className='icon'/>
                 </div>
-                <div className="contact email">
+                <div className="contact email" ref={emailButton} onClick={()=> handleClick(emailButton.current.title)} title='email'>
                     <p>email</p>
                     <FontAwesomeIcon icon={faEnvelope} id="email" className='icon'/>
                 </div>
-                <div className="contact linkedin">
+                <div className="contact linkedin" ref={linkeinButton} onClick={()=> handleClick(linkeinButton.current.title)} title='linkedin'>
                     <p>linkedIN</p>
                     <FontAwesomeIcon icon={faLinkedin} id="linkedin" className='icon'/>
                 </div>
-                <div className="contact github">
+                <div className="contact github" ref={githubButton} onClick={()=> handleClick(githubButton.current.title)} title='github'>
                     <p>github</p>
                     <FontAwesomeIcon icon={faGithubSquare} id="github" className='icon'/>
                 </div>
--- public/components/nav-menu/styles.ts
+++ public/components/nav-menu/styles.ts
@@ -21,15 +21,16 @@ export const Container = styled.div`
             text-decoration: none;
             color: #fff;
             font-family: 'Roboto', sans-serif;
-            font-weight: 500;
+            font-weight: 300;
             text-shadow: 2px 2px 1px rgba(0, 0, 0, 0.5);
             transition: all .2s ease;
             border-left: 1px solid #fff;
 
             :hover {
                 border-left: 3px solid #AF5CBC;
-                transform: scale(1.1);
                 color: #AF5CBC;
+                transform: translateX(10px);
+                font-weight: 500;
             }
         }
     }
