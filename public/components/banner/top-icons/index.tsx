33ffaefd18ca06a99db044622d8fb6b9dd08d0b5
hash-luk <lucasgamerhd18@gmail.com>
2022-04-14 10:03:50 -0300

    Added animation to top-menu icons

--- public/components/banner/top-icons/index.tsx
+++ public/components/banner/top-icons/index.tsx
@@ -5,17 +5,34 @@ import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const TopIcons = () => {
+    const variantscontainer = {
+        hidden: {
+            opacity: 0,
+            y: -100
+        },
+        visible: {
+            opacity: 1,
+            y: 0
+        }
+    }
+
     return(
         <S.Container>
-            <a href="https://github.com/hash-luk">
-                <FontAwesomeIcon icon={faGithub} id="icon" className='github-icon' size='2x' inverse/>
-            </a>
-            <a href="https://instagram.com/lucasp_dev">
-                <FontAwesomeIcon icon={faInstagram} id="icon" className='instagram-icon' size='2x' inverse/>
-            </a>
-            <a href="https://linkedin.com/in/lucas-patrick-p">
-                <FontAwesomeIcon icon={faLinkedin} id="icon" className='linkedin-icon' size='2x' inverse/>
-            </a>
+            <S.Icon variants={variantscontainer} initial="hidden" animate="visible" transition={{duration: 1, bounce:0.2,type:"spring"}}>
+                <a href="https://github.com/hash-luk">
+                    <FontAwesomeIcon icon={faGithub} id="icon" className='github-icon' size='2x' inverse/>
+                </a>
+            </S.Icon>
+            <S.Icon variants={variantscontainer} initial="hidden" animate="visible" transition={{duration: 1, bounce:0.2,type:"spring",delay:0.2}}>
+                <a href="https://instagram.com/lucasp_dev">
+                    <FontAwesomeIcon icon={faInstagram} id="icon" className='instagram-icon' size='2x' inverse/>
+                </a>
+            </S.Icon>
+            <S.Icon variants={variantscontainer} initial="hidden" animate="visible" transition={{duration: 1, bounce:0.2,type:"spring",delay:0.4}}>
+                <a href="https://linkedin.com/in/lucas-patrick-p">
+                    <FontAwesomeIcon icon={faLinkedin} id="icon" className='linkedin-icon' size='2x' inverse/>
+                </a>
+            </S.Icon>
         </S.Container>
     )
 }
