4e2deaedbcc9a147581a56d03ec3baf023d8b9d3
hash-luk <lucasgamerhd18@gmail.com>
2022-04-15 00:34:22 -0300

    Added animations to about me section

--- public/components/about-me/index.tsx
+++ public/components/about-me/index.tsx
@@ -1,7 +1,9 @@
 import * as S from "./styles";
 import Image from "next/image";
-import { MutableRefObject, useEffect, useRef } from "react";
+import { MutableRefObject, useEffect, useRef,useState } from "react";
+import { useInView } from "react-intersection-observer";
 import profilePhoto from "../../../public/assets/images/profile-photo.svg";
+import { useViewportScroll, useAnimation } from "framer-motion";
 
 const AboutMe = () => {
   const ball1 = useRef() as MutableRefObject<HTMLDivElement>;
@@ -13,27 +15,96 @@ const AboutMe = () => {
 
   const balls = [ball1, ball2, ball3, ball4, ball5, ball6];
 
+
+
+  const titleAnimation = {
+    hidden: {
+      x: -20,
+      opacity: 0,
+    },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 2,
+        type:'tween',
+      }
+    }
+  }
+
+  const textAnimaion = {
+    hidden: {
+      opacity: 0
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 1,
+        delay: 0.2,
+        stiffness: 100,
+      }
+    }
+  }
+
+  const textBoxAnimation = {
+    hidden: {
+      width: 0,
+      backgroundColor: 'transparent',
+      border: 'none',
+    },
+    visible: {
+      width: '55rem',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      border: '1px solid #fff',
+
+      transition: {
+        duration: 1,
+        type:"tween",
+        stiffness: 100,
+        clamp: false,
+      }
+    }
+  }
+
+
+
   useEffect(() => {
-    console.log("rodou");
     balls.forEach((ball) => {
-      ball.current.style.top = `${Math.random() * 100}%`;
-      ball.current.style.left= `${Math.random() * 10}%`;
-      ball.current.style.gap = `${Math.random() * 100}%`;
+      ball.current.style.top = `${Math.random() * 100}vw`;
+      ball.current.style.left = `${Math.random() * 100}%`;
+      ball.current.style.gap = `${Math.random() * 30}%`;
     });
-  });
+  },[]);
+
 
   return (
     <S.Container id="aboutme">
-<div className="content">
-      <div className="profilePhoto">
-        <Image src={profilePhoto} alt="profilePhoto" width={310} height={310} />
-      </div>
-      <h2>SOBR<span>E</span></h2>
-      <div className="content-text">
-        <p>
-        Sou o Lucas Patrick, desenvolvedor front-end localizado em Belo Horizonte/MG. Desde criança sempre fui apaixonado por design e tecnologia então desde 2018 decidi investir na área. Iniciei os aprendizados em HTML e CSS, posteriormente aprendendo o básico de JavaScript. Atualmente cursando análise e desenvolvimento de sistemas e como principal stack o React, mas sou bastante flexivel quanto a tecnologia e tenho rápido aprendizado. Sou focado, determinado, disciplinado e adoro aprender com as pessoas que possuem mais eperiência . Amo trabalhar em equipe pois acho que é mais incentivante.
-        </p>
-      </div>
+      <div className="content">
+        <div className="profilePhoto">
+          <Image
+            src={profilePhoto}
+            alt="profilePhoto"
+            width={310}
+            height={310}
+          />
+        </div>
+          <S.Title variants={titleAnimation} initial="hidden" whileInView='visible' viewport={{once:true}}>
+            SOBR<span>E</span>
+          </S.Title>
+        <S.ContentText className="content-text" variants={textBoxAnimation} initial="hidden" whileInView='visible' viewport={{once:true}}>
+          <S.Text variants={textAnimaion} initial="hidden" whileInView='visible'  viewport={{once:true}}>
+            Sou o Lucas Patrick, desenvolvedor front-end localizado em Belo
+            Horizonte/MG. Desde criança sempre fui apaixonado por design e
+            tecnologia então desde 2018 decidi investir na área. Iniciei os
+            aprendizados em HTML e CSS, posteriormente aprendendo o básico de
+            JavaScript. Atualmente cursando análise e desenvolvimento de
+            sistemas e como principal stack o React, mas sou bastante flexivel
+            quanto a tecnologia e tenho rápido aprendizado. Sou focado,
+            determinado, disciplinado e adoro aprender com as pessoas que
+            possuem mais eperiência . Amo trabalhar em equipe pois acho que é
+            mais incentivante.
+          </S.Text>
+        </S.ContentText>
       </div>
 
       <div className="ball" ref={ball1}></div>
--- public/components/about-me/styles.ts
+++ public/components/about-me/styles.ts
@@ -1,167 +1,171 @@
+import { motion } from "framer-motion";
 import styled from "styled-components";
 
-
 export const Container = styled.div`
-    width: 100vw;
-    height: 100vh;
-    position: relative;
-    overflow: hidden;
-    background-color: #AF5CBC;
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  overflow: hidden;
+  background-color: #af5cbc;
+  display: flex;
+  font-family: "Roboto", sans-serif;
+  text-shadow: 2px 2px 1px rgba(0, 0, 0, 0.5);
+  padding: 20px 0px;
+
+  .content {
+    position: absolute;
     display: flex;
-    font-family: 'Roboto', sans-serif;
-    text-shadow: 2px 2px 1px rgba(0, 0, 0, 0.5);
-    padding: 20px 0px;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    width: 100%;
+    height: 100%;
+    z-index: 4;
+  }
 
-    .content {
-        position: absolute;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: space-evenly;
-        width: 100%;
-        height: 100%;
-        z-index: 4;
+  .profilePhoto {
+    width: 13.875rem;
+    height: 13.438rem;
+    z-index: 4;
+    transition: all 0.2s ease;
+    border-radius: 50%;
 
-        h2 {
-            font-size:3rem;
-            line-height: 3.5rem;
-            text-align: right;
-            color: #DDDDDD;
-            margin-bottom: 1.875rem;
-            transition: all .2s ease-in-out;
-            user-select: none;
-            -webkit-user-select: none;
-
-            span {
-                color: #303030;
-                transition: all .2s ease-in-out;
-            }
-
-            :hover {
-                transform: scale(1.1);
-                letter-spacing: .1rem;
-
-                span {
-                    color: #DDDDDD;
-                }
-            }
-        }
+    :hover {
+      transform: scale(1.1);
     }
+  }
 
+  .ball {
+    background-color: #303030;
+    position: absolute;
+    z-index: 1;
+    width: 100px;
+    height: 100px;
+    border-radius: 100%;
+    overflow: hidden !important;
+    animation: scaleAnimation 3s infinite;
+
+    @keyframes scaleAnimation {
+      0% {
+        transform: scale(1);
+      }
+      50% {
+        transform: scale(1.1);
+      }
+      100% {
+        transform: scale(1);
+      }
+    }
+  }
+
+  @media screen and (max-width: 768px) {
     .profilePhoto {
-        width: 13.875rem;
-        height: 13.438rem;
-        z-index: 4;
-        transition: all .2s ease;
-        border-radius: 50%;
-
-        :hover {
-            transform: scale(1.1);
-        }
-    }
-
-    .content-text {
-        width: 55rem;
-        height: 14rem;
-        font-weight: 400;
-        text-align: center;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-
-        background-color: rgba(0, 0, 0, 0.5);
-        border: 0.5px solid #DADADADA;
-        border-radius: 15px;
-        transition: all .2s ease;
-
-        span {
-            font-size: 1.8rem;
-        }
-
-        p {
-            max-width: 53rem;
-            max-height: 13.188rem;
-            color: #f2f2f2;
-            font-size: 1.3rem;
-            line-height: 24px;
-        }
-
-        :hover {
-            background-color: rgba(0, 0, 0, 0.8);
-        }
+      width: 10rem;
+      height: 10.2rem;
     }
 
     .ball {
-        background-color: #303030;
-        position: absolute;
-        z-index: 1;
-        width: 100px;
-        height: 100px;
-        border-radius: 100%;
-        overflow: hidden !important;
-        animation: scaleAnimation 3s infinite;
-
-
-        @keyframes scaleAnimation {
-            0% {
-                transform: scale(1);
-            }
-            50% {
-                transform: scale(1.1);
-            }
-            100% {
-                transform: scale(1);
-            }
-        }
+      display: none;
     }
+  }
+
+  @media screen and (max-width: 600px) {
+    .profilePhoto {
+      width: 10rem;
+      height: 10.2rem;
+    }
+
+    .ball {
+      display: none;
+    }
+  }
+`;
+
+export const Title = styled(motion.h2)`
+  font-size: 3rem;
+  line-height: 3.5rem;
+  text-align: center;
+  color: #dddddd;
+  margin-bottom: 1.875rem;
+  transition: all 0.2s ease-in-out;
+  user-select: none;
+  -webkit-user-select: none;
+
+  span {
+    color: #303030;
+    transition: all 0.2s ease-in-out;
+  }
+
+  :hover {
+    transform: scale(1.1);
+    letter-spacing: 0.1rem;
+
+    span {
+      color: #dddddd;
+    }
+  }
+
+  @media screen and (max-width: 600px) {
+    font-size: 2rem !important;
+  }
+`;
+
+export const ContentText = styled(motion.div)`
+  width: 55rem;
+  height: 14rem;
+  font-weight: 400;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: rgba(0, 0, 0, 0.5);
+  border: 0.5px solid #dadadada;
+  border-radius: 15px;
+  transition: all 0.2s ease;
+
+  p {
+
+  }
+
+  :hover {
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 95%;
+    height: 20rem;
+    border: none;
+  }
+
+  @media screen and (max-width: 600px) {
+    width: 95%;
+    height: 20rem;
+    border: none;
+  }
+`;
+
+export const Text = styled(motion.p)`
+    max-width: 53rem;
+    max-height: 13.188rem;
+    color: #f2f2f2;
+    font-size: 1.3rem;
+    line-height: 24px;
+    transition: all 4s ease;
+    animation: opacityTransition 10s ease forwards;
+
+
 
     @media screen and (max-width: 768px) {
-        .profilePhoto {
-            width: 10rem;
-            height: 10.2rem;
-        }
-
-        .content-text {
-            width: 95%;
-            height: 20rem;
-            border: none;
-
-            p {
-                max-width: 90%;
-                font-size: 1rem;
-                text-align: left;
-            }
-        }
-
-        .ball {
-            display: none;
-        }
+      max-width: 90%;
+      font-size: 1rem;
+      text-align: left;
     }
 
-    @media screen and (max-width:600px) {
-        .profilePhoto {
-            width: 10rem;
-            height: 10.2rem;
-        }
 
-        h2 {
-            font-size: 2rem !important;
-        }
-
-        .content-text {
-            width: 95%;
-            height: 20rem;
-            border: none;
-
-            p {
-                max-width: 90%;
-                font-size: 1rem;
-                text-align: left;
-            }
-        }
-
-        .ball {
-            display: none;
-        }
+    @media screen and (max-width: 600px) {
+      max-width: 90%;
+      font-size: 1rem;
+      text-align: left;
     }
-`;
+`
