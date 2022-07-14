76e9b4a3c1c6193b433cc2077230263e8a398808
hash-luk <lucasgamerhd18@gmail.com>
2022-07-14 12:00:43 -0300

    Added widget button styles

--- public/components/feedget/index.tsx
+++ public/components/feedget/index.tsx
@@ -1,25 +1,23 @@
 import { ChatTeardropDots } from "phosphor-react";
-import { Popover } from "@headlessui/react";
+// import { Popover } from "@headlessui/react";
 import WidgetForm from "./feedget-components/WidgetForm";
 import * as S from "./styles";
 
 const Feedget = () => {
   return (
     <S.Container>
-      <Popover className="absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end">
-        <Popover.Panel>
+        <S.Container.Panel>
           <WidgetForm />
-        </Popover.Panel>
+        </S.Container.Panel>
 
-        <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
-          <ChatTeardropDots className="w-6 h-6" />
+        <S.Container.Button className="pop-button">
+          <ChatTeardropDots className="chat-icon" />
           <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
             <span className="pl-2 "></span>
             Feedback
           </span>
-        </Popover.Button>
-      </Popover>
-    </S.Container>
+        </S.Container.Button>
+      </S.Container>
   );
 };
 
--- public/components/feedget/styles.ts
+++ public/components/feedget/styles.ts
@@ -1,9 +1,33 @@
 import styled from "styled-components";
+import { Popover } from "@headlessui/react";
 
-export const Container = styled.div`
+export const Container = styled(Popover)`
     position: fixed;
-    right: 4px;
-    bottom: 50px;
+    right: 3.125rem;
+    bottom: 3.125rem;
+    width: fit-content;
     background-color: red;
     z-index: 999;
+    display: flex;
+    align-items: flex-end;
+    flex-direction: column;
+    background-color: #af5cbc;
+    border-radius: 5px;
+    font-family: "Roboto", sans-serif;
+
+    .pop-button {
+        color: #fff;
+        width: 130px;
+        height: 30px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 30px;
+        font-weight: 400;
+    }
+
+    .chat-icon {
+        width: 20px;
+        height: 20px;
+    }
 `;
