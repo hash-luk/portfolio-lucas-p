9261dc133a7f7b29d37b2245769cfecfddd16cb9
hash-luk <lucas.patrick2506@gmail.com>
2023-12-06 10:18:06 -0300

    fix: Correct title on pages

--- public/components/nav-menu/index.tsx
+++ public/components/nav-menu/index.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import {
   Box,
@@ -8,91 +8,131 @@ import {
   useDisclosure,
   useColorModeValue,
   Stack,
-} from '@chakra-ui/react'
+} from "@chakra-ui/react";
 
-import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons'
-import Image from 'next/image'
-import { handleRedirect,handleDownload } from "../../../utils/functions";
-import Logo from "../../assets/icons/logo.svg"
+import { CloseIcon, HamburgerIcon } from "@chakra-ui/icons";
+import Image from "next/image";
+import { handleRedirect, handleDownload } from "../../../utils/functions";
+import Logo from "../../assets/icons/logo.svg";
 
 interface Props {
-  children: React.ReactNode,
-  href: string
+  children: React.ReactNode;
+  href: string;
 }
 
 type MenuItem = {
-  nome: string,
-  href: string
-}
+  nome: string;
+  href: string;
+};
 
 const links: MenuItem[] = [
-  { nome: 'Sobre', href: 'aboutme' },
-  { nome: 'Conhecimentos', href: 'techstack' },
-  { nome: 'Projetos', href: 'projects' },
-  { nome: 'Currículo', href: "./assets/docs/curriculum.pdf"}
+  { nome: "Sobre", href: "aboutme" },
+  { nome: "Conhecimentos", href: "techstack" },
+  { nome: "Projetos", href: "projects" },
+  { nome: "Currículo", href: "./assets/docs/curriculum.pdf" },
 ];
 
-const NavLink = (props : Props) => {
-  const {children, href} = props;
+const NavLink = (props: Props) => {
+  const { children, href } = props;
 
-  return(
-    <Box 
-      as='a' 
-      px={2} 
-      py={1} 
-      rounded={"md"} 
+  return (
+    <Box
+      as="a"
+      px={2}
+      py={1}
+      rounded={"md"}
       _hover={{
-        textDecoration: 'none',
-        color: useColorModeValue('customBlue.600', 'customBlue.500')
+        textDecoration: "none",
+        color: useColorModeValue("customBlue.600", "customBlue.500"),
       }}
-      color={useColorModeValue('customGray.100', 'customGray.100')}
+      color={useColorModeValue("customGray.100", "customGray.100")}
       cursor={"pointer"}
-      onClick={() => children !== "Currículo" ? handleRedirect(href, "self") : handleDownload(href)}
+      onClick={() =>
+        children !== "Currículo"
+          ? handleRedirect(href, "self")
+          : handleDownload(href)
+      }
     >
       {children}
     </Box>
-  )
-}
+  );
+};
 
 export default function NavBar() {
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-  return(
+  return (
     <>
-      <Box bg={useColorModeValue('customGray.500', 'customGray.500')} px={4} width={'100vw'} paddingX={{lg: "6", md: "6", sm: "6", base: "6"}}>
-        <Flex h={32} alignItems={'center'} justifyContent={'space-between'} maxW={'1220px'} margin={'0 auto'} w={'full'}>
-          <HStack spacing={8} alignItems={'center'} justifyContent={'space-between'} w={'100%'}>
-            <Box as='a' onClick={() => {window.location.reload()}} cursor={"pointer"}>
-              <Image src={Logo} alt='Logo com formato de tag HTMl em azul formando um L no meio' style={{borderRadius: "8px"}}/>
+      <Box
+        bg={useColorModeValue("customGray.500", "customGray.500")}
+        px={4}
+        width={"100vw"}
+        paddingX={{ lg: "6", md: "6", sm: "6", base: "6" }}
+      >
+        <Flex
+          h={32}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+          maxW={"1220px"}
+          margin={"0 auto"}
+          w={"full"}
+        >
+          <HStack
+            spacing={8}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+            w={"100%"}
+          >
+            <Box
+              as="a"
+              onClick={() => {
+                window.location.reload();
+              }}
+              cursor={"pointer"}
+            >
+              <Image
+                src={Logo}
+                alt="Logo com formato de tag HTMl em azul formando um L no meio"
+                style={{ borderRadius: "8px" }}
+              />
             </Box>
-            <HStack as={'nav'} spacing={12} display={{base: 'none', md:'flex'}}>
-              {links.map((item,index) => (
-                <NavLink key={index} href={item.href}>{item.nome}</NavLink>
+            <HStack
+              as={"nav"}
+              spacing={12}
+              display={{ base: "none", md: "flex" }}
+            >
+              {links.map((item, index) => (
+                <NavLink key={index} href={item.href}>
+                  {item.nome}
+                </NavLink>
               ))}
             </HStack>
           </HStack>
-          <IconButton 
-            size={'md'}
+          <IconButton
+            size={"md"}
             icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={'Abrir menu'}
-            display={{md: "none"}}
+            aria-label={"Abrir menu"}
+            display={{ md: "none" }}
             onClick={isOpen ? onClose : onOpen}
           />
         </Flex>
-        {
-          isOpen ? (
-            <Flex pb={4} display={{md: 'none'}} justifyContent={'flex-end'} textAlign={'right'}>
-              <Stack as={'nav'} spacing={4}>
-                {
-                  links.map((item,index) => (
-                    <NavLink key={index} href={item.href}>{item.nome}</NavLink>
-                  ))
-                }
-              </Stack>
-            </Flex>
-          ) : null
-        }
+        {isOpen ? (
+          <Flex
+            pb={4}
+            display={{ md: "none" }}
+            justifyContent={"flex-end"}
+            textAlign={"right"}
+          >
+            <Stack as={"nav"} spacing={4}>
+              {links.map((item, index) => (
+                <NavLink key={index} href={item.href}>
+                  {item.nome}
+                </NavLink>
+              ))}
+            </Stack>
+          </Flex>
+        ) : null}
       </Box>
     </>
-  )
+  );
 }
