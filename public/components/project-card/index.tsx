7edb0b64707d7d14a4d569f7851379288733892f
hash-luk <lucas.patrick2506@gmail.com>
2023-12-12 12:04:54 -0300

    feat: Created new layout and component Project Card

---
+++ public/components/project-card/index.tsx
@@ -0,0 +1,79 @@
+import {
+  Box,
+  Button,
+  HStack,
+  Text,
+  VStack,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import Image from "next/image";
+
+interface ProjectProps {
+  projectName: string;
+  projectImage: string;
+  techs: Array<string>;
+  deployLink: string;
+  repoLink: string;
+}
+
+export default function ProjectCard(props: ProjectProps) {
+  const imgSrc = `/assets/images/projects/${props.projectImage}`;
+
+  return (
+    <VStack width={"500px"} h={"360"} gap={0}>
+      <Box
+        w={"500px"}
+        h={"190"}
+        position={"relative"}
+        borderTopLeftRadius={8}
+        borderTopRightRadius={8}
+        overflow={"hidden"}
+      >
+        <Image
+          src={imgSrc}
+          alt={`Imagem do projeto ${props.projectName}`}
+          fill
+          style={{ objectFit: "cover" }}
+        />
+      </Box>
+      <HStack
+        w={"full"}
+        borderBottomLeftRadius={8}
+        borderBottomRightRadius={8}
+        padding={8}
+        justifyContent={"center"}
+        backgroundColor={useColorModeValue(
+          "customBgColors.gray100",
+          "customBgColors.gray100"
+        )}
+      >
+        <VStack gap={8}>
+          <Text
+            as={"h5"}
+            fontSize={"lg"}
+            fontWeight={"bold"}
+            color={useColorModeValue(
+              "grayChateau.200",
+              "grayChateau.200"
+            )}
+          >
+            {props.projectName}
+          </Text>
+          <HStack>
+            {props.techs.map((item, index) => (
+              <Text as={"span"} fontSize={"sm"} key={index}>{item} {index != props.techs.length - 1 ? "•" : ""}</Text>
+            ))}
+          </HStack>
+          <HStack gap={8}>
+            <Button as={"a"} href={props.deployLink} padding={6}>
+              Visualizar na WEB
+            </Button>
+            <Button as={"a"} href={props.repoLink} padding={6}>
+              Visualizar repositório
+            </Button>
+          </HStack>
+        </VStack>
+      </HStack>
+    </VStack>
+  );
+}
---
+++ utils/data/projects.ts
@@ -0,0 +1,56 @@
+export const projects = [
+  {
+    title: "Spider-Man: Miles Morales",
+    image: "spider.png",
+    techs: ["JavaScript", "HTML5", "SASS", "JQuery", "TiltJS"],
+    links: {
+      deploy: "https://spider-man-home-page.vercel.app/",
+      repository: "https://github.com/hash-luk/SpiderMan-Home-Page",
+    },
+  },
+  {
+    title: "BMW",
+    image: "bmw.png",
+    techs: ["HTML", "CSS", "JavaScript", "SASS", "NodeJS"],
+    links: {
+      deploy: "https://bmw-project.vercel.app/",
+      repository: "https://github.com/hash-luk/BMW-Project",
+    },
+  },
+  {
+    title: "DevFinance",
+    image: "devfinance.png",
+    techs: ["JavaScript", "HTML", "SASS", "JQuery"],
+    links: {
+      deploy: "https://dev-finance-jrsq2ybxk-hash-luk.vercel.app/",
+      repository: "https://github.com/hash-luk/Dev-Finance",
+    },
+  },
+  {
+    title: "Madeireira Mogno",
+    image: "madeireira.png",
+    techs: ["React", "TypeScript", "NodeJS", "Framer Motion", "Twillio"],
+    links: {
+      deploy: "https://madeireira-mogno.vercel.app/",
+      repository: "https://github.com/hash-luk/madeireira-mogno",
+    },
+  },
+  {
+    title: "Move.IT",
+    image: "moveit.png",
+    techs: ["NextJS", "TypeScript", "Firebase", "Modular CSS"],
+    links: {
+      deploy: "https://keep-in-action.vercel.app/",
+      repository: "https://github.com/hash-luk/Move.it",
+    },
+  },
+  {
+    title: "Netflix UI Clone",
+    image: "netflix.png",
+    techs: ["React", "Material UI", "JavaScript", "NodeJS", "TMDB API"],
+    links: {
+      deploy: "https://netflix-ui-clone-lilac.vercel.app/",
+      repository: "https://github.com/hash-luk/Netflix-UI-Clone",
+    },
+  },
+];
