4cda7602f1c247ba8525f71958bb655aede7c52a
hash-luk <lucas.patrick2506@gmail.com>
2024-03-08 18:24:03 -0300

    fix: Remove unecessary links on footer

--- public/components/footer/index.tsx
+++ public/components/footer/index.tsx
@@ -22,44 +22,17 @@ const Footer = () => {
       <HStack
         maxW={"1220px"}
         margin={"0 auto"}
-        justifyContent={"space-between"}
+        justifyContent={"center"}
         flex={1}
-        flexDirection={{
-          lg: "row",
-          md: "row",
-          sm: "column-reverse",
-          base: "column-reverse",
-        }}
+        flexDirection={"row"}
       >
         <Text
           as={"p"}
           color={useColorModeValue("grayChateau.200", "grayChateau.200")}
-          fontSize={{lg: "lg", md: "lg", sm: "md", base: "md"}}
+          fontSize={{ lg: "lg", md: "lg", sm: "md", base: "md" }}
         >
           2023&copy; lucascode.dev - Todos os direitos reservados.
         </Text>
-        <HStack gap={8}>
-          <Link isExternal href="https://github.com/hash-luk">
-            <Text
-              color={useColorModeValue("customGray.100", "customGray.100")}
-              _hover={{
-                color: useColorModeValue("grayChateau.500", "grayChateau.500"),
-              }}
-            >
-              <AiFillGithub size={36} />
-            </Text>
-          </Link>
-          <Link isExternal href="https://linkedin.com/in/lucas-patrick-p">
-            <Text
-              color={useColorModeValue("customGray.100", "customGray.100")}
-              _hover={{
-                color: useColorModeValue("customBlue.500", "customBlue.500"),
-              }}
-            >
-              <AiFillLinkedin size={36} />
-            </Text>
-          </Link>
-        </HStack>
       </HStack>
     </Flex>
   );
